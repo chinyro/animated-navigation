ee5ee3978b1af7ec9fd3204e7f64d66f5f624720
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-03-05 22:26:47 -0600

    adding class styles

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+
+function toggleNav() {
+    // Toggle: Menu Bars Open/Close
+    menuBars.classList.toggle('change');
+}
+
+
+// Event listeners
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
+
